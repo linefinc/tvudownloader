13d9d0f6dd9c463510c16461cb23de57c6a76e77
linefinc <linefinc@4e9d9500-3722-a143-8048-b556883094bb>
2010-06-16 12:22:31 +0000

    Add support for Notify
    git-svn-id: svn://svn.code.sf.net/p/tvudownloader/code@2 4e9d9500-3722-a143-8048-b556883094bb

--- tvu/Form1.cs
+++ tvu/Form1.cs
@@ -23,18 +23,109 @@ namespace tvu
         private string ServiceUrl;
         public int IntervalTime;
 
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenu contextMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+
+
         public List<RssFeed> RssFeedList;
 
         public Form1()
         {
+
+
             RssFeedList = new List<RssFeed>();
             
             InitializeComponent();
             InitConfig();
             LoadConfig();
-            //GetConfigRSSList(());
-            
 
+            SetupNotify();
+
+
+
+
+
+
+        }
+
+
+        private void SetupNotify()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+
+            // Initialize contextMenu1
+            this.contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { this.menuItem1, this.menuItem2 });
+
+            // Initialize menuItem1
+            this.menuItem1.Index = 0;
+            this.menuItem1.Text = "E&xit";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+
+            this.menuItem2.Index = 1;
+            this.menuItem2.Text = "Hide";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem1_Prova_Click);
+
+
+            // Create the NotifyIcon.
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+
+            // The Icon property sets the icon that will appear
+            // in the systray for this application.
+            notifyIcon1.Icon = new Icon("appicon2.ico");
+
+            // The ContextMenu property sets the menu that will
+            // appear when the systray icon is right clicked.
+            notifyIcon1.ContextMenu = this.contextMenu1;
+
+            // The Text property sets the text that will be displayed,
+            // in a tooltip, when the mouse hovers over the systray icon.
+            notifyIcon1.Text = "Form1 (NotifyIcon example)";
+            notifyIcon1.Visible = true;
+
+            // Handle the DoubleClick event to activate the form.
+            notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+
+        }
+        private void menuItem1_Prova_Click(object Sender, EventArgs e)
+        {
+            if (this.Visible == true)
+            {
+                this.Hide();
+                this.contextMenu1.MenuItems[1].Text = "Show";
+                notifyIcon1.Icon = new Icon("appicon1.ico");
+
+            }
+            else
+            {
+                this.Show();
+                this.contextMenu1.MenuItems[1].Text = "Hide";
+                notifyIcon1.Icon = new Icon("appicon2.ico");
+            }
+
+        }
+
+        private void notifyIcon1_DoubleClick(object Sender, EventArgs e)
+        {
+            // Show the form when the user double clicks on the notify icon.
+
+            // Set the WindowState to normal if the form is minimized.
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+
+            // Activate the form.
+            this.Activate();
+        }
+
+        private void menuItem1_Click(object Sender, EventArgs e)
+        {
+            // Close the form, which closes the application.
+            this.Close();
         }
 
         private void LoadConfig()
@@ -831,6 +922,9 @@ namespace tvu
             //string appPath = Environment.GetFolderPath(Path.GetDirectoryName(Application.ExecutablePath);
             string appPath;
             appPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+
+            
 
         }
 
