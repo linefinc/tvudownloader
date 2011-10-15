d5e14e65e5cd5b4af99f5b42b4f1862ea6d7217f
linefinc <linefinc@4e9d9500-3722-a143-8048-b556883094bb>
2011-10-15 09:28:35 +0000

    Fix About box with license and correct caption
    git-svn-id: svn://svn.code.sf.net/p/tvudownloader/code@124 4e9d9500-3722-a143-8048-b556883094bb

--- tvu/AboutBox1.Designer.cs
+++ tvu/AboutBox1.Designer.cs
@@ -150,7 +150,7 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "AboutBox1";
+            this.Text = "About";
             this.tableLayoutPanel.ResumeLayout(false);
             this.tableLayoutPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
--- tvu/AboutBox1.cs
+++ tvu/AboutBox1.cs
@@ -17,7 +17,22 @@ namespace tvu
             this.labelVersion.Text = Config.Version.ToString();
             this.labelCopyright.Text = "GNU General Public License 3";
 
-            this.textBoxDescription.Text = "";
+            this.textBoxDescription.Text = @"TV Undergroud Downloader
+
+Copyright (C) 2011  linefinc[at]users.sourceforge.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.";
         }
 
         #region Assembly Attribute Accessors
