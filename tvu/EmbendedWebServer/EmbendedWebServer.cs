512a454572e4d8f7457a0d2bf25a560e84711915
linefinc <linefinc@gmail.com>
2017-02-04 20:39:21 +0100

    Add Nancyfx

---
+++ tvu/EmbendedWebServer/EmbendedWebServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TvUndergroundDownloader.EmbendedWebServer
+{
+    class EmbendedWebServer
+    {
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+    }
+}
---
+++ tvu/EmbendedWebServer/MainModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TvUndergroundDownloader.EmbendedWebServer
+{
+    class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ =>
+            
+                 "Hello World";
+            
+        }
+    }
+}
