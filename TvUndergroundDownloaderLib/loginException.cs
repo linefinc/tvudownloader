277d82a851cbf62f5e3c0cb00fb7019ca52ab56a
linefinc <linefinc@gmail.com>
2017-06-11 22:32:38 +0200

    Improve Login Exception

--- TvUndergroundDownloaderLib/loginException.cs
+++ TvUndergroundDownloaderLib/loginException.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace TvUndergroundDownloaderLib
 {
+    [Serializable]
     class LoginException : Exception
     {
-        public LoginException(): base("Login Error")
+        public LoginException()
         {
 
         }
+
+        public LoginException(string message) : base(message)
+        {
+
+        }
+
+        public LoginException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+
     }
 }
